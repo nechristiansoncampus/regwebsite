926327919060804c9a825baa4c82061305925c49
David Tien <david@dtien-air.local>
2022-02-25 23:17:25 -0500

    add static files, add reminder to screenshot, update to production spreadsheet

--- home.py
+++ home.py
@@ -14,27 +14,21 @@ def home():
 @app.route("/info", methods=['post', 'get'])
 def info():
     gc = pygsheets.authorize(service_account_env_var='service_credentials')
-    sh = gc.open('Testing Spreadsheet')
+    sh = gc.open('2022 Spring Retreat - Registration & Planning')
     wks = sh.worksheet_by_title('Responses')
 
     if request.method == 'POST':
-        # print("post")
-        # print(request.form)
         user = []
         i = 0
         for row in wks:
             i += 1
-            # print(row[2], row[3], row[7].translate(str.maketrans('', '', '-')))
-            sheet_pn = re.sub(r"\D", "", row[7])
+            sheet_pn = re.sub(r"\D", "", row[10])
             form_pn = request.form['phone']
             if sheet_pn == form_pn:
-                # print(row[2], row[3])
-                # print(row[7])
                 if (row[4].strip() == ''):
                     return render_template('not_paid.html', name=row[2] + ' ' + row[3])
                 else:
                     wks.update_value('B' + str(i), True)
-                    return render_template('info.html', name=row[2] + ' ' + row[3])
+                    return render_template('info.html', name=row[2] + ' ' + row[3], housing=row[6], activity_group=row[7])
                 break
-        # print(wks.get_row(i))
     return render_template('home.html', error="Phone number not registered")
